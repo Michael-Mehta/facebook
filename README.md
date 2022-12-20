44a7002dec3df8a67a51686bdc9f8743e877121e
Michael-Mehta <supermichael2001@gmail.com>
2022-12-19 17:36:43 -0700

    created the users index page and added the globe icon to the nav bar that navigates to the index

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # README
 
+
 Rails facebook project for The Odin Project, for more information visit https://www.theodinproject.com/lessons/ruby-on-rails-rails-final-project
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,30 @@
 class UsersController < ApplicationController
     before_action :authenticate_user!
-    before_action :set_user
+ 
 
     def show
-       
+        @user = User.find(params[:id])
     end
 
     def index
+        @users = User.all
     end
 
     def friend
+        @user = User.find(params[:id])
         current_user.send_follow_request_to(@user)
         redirect_to root_path
     end
 
     def unfriend
+        @user = User.find(params[:id])
         current_user.unfollow(@user)
         @user.unfollow(current_user)
         redirect_to root_path
     end
 
     def accept
+        @user = User.find(params[:id])
         current_user.accept_follow_request_of(@user)
         current_user.send_follow_request_to(@user)
         @user.accept_follow_request_of(current_user)
@@ -28,21 +32,20 @@ class UsersController < ApplicationController
     end
 
     def decline
+        @user = User.find(params[:id])
         current_user.decline_follow_request_of(@user)
         redirect_to root_path
     end
 
     def cancel
+        @user = User.find(params[:id])
         current_user.remove_follow_request_for(@user)
         redirect_to root_path
     end
 
-    private
+  
 
-    def set_user
-        @user = User.find(params[:id])
-
-    end
+    
 
 
 end
